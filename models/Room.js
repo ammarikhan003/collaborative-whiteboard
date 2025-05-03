4943791ab3e36ec6bdaf7ea98ab485afc586591d
Ammar Khan <ammar.ikhan003@gmail.com>
2025-05-04 00:07:31 +0100

    isPrivate addition

--- models/Room.js
+++ models/Room.js
@@ -16,6 +16,10 @@ const RoomSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
+  isPrivate: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
